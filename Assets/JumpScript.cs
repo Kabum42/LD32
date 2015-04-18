6164820f1c7d2e7f5345ff54ae708851ee05abe8
EtherealWhisper <yomowilce@gmail.com>
2015-04-18 14:18:54 +0200

    Initial JumpScript

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpScript : MonoBehaviour {
+
+	Rigidbody my_rigidbody;
+	float jumpMagnitude = 20000f;
+	bool canJump = true;
+	float hitDist = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+
+		my_rigidbody = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Space) && canJump) {
+
+			my_rigidbody.AddForce (Vector3.up * jumpMagnitude);
+			canJump = false;
+		}
+
+		if (Physics.Raycast (transform.position, -Vector3.up, hitDist)) {
+
+			canJump = true;
+		}
+	}
+}
