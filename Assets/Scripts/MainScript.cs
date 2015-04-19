bfc0fd7f68ce0b93278c84d84e3f0ffa38ebddd7
Kabum42 <al264084@uji.es>
2015-04-19 18:30:15 +0200

    MainMenu

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -252,7 +252,7 @@ public class MainScript : MonoBehaviour {
 		
 		//Facing();
 
-		Highlight ();
+
 		
 	}
 	
@@ -268,31 +268,7 @@ public class MainScript : MonoBehaviour {
 		
 	}
 
-	void Highlight() {
 
-		Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		
-		// Create a RaycastHit variable to store information about what was hit by the ray.
-		RaycastHit floorHit;
-		
-		// Perform the raycast and if it hits something on the floor layer...
-		if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
-		{
-			// Create a vector from the player to the point on the floor the raycast from the mouse hit.
-			Vector3 playerToMouse = floorHit.point - player.transform.position;
-			
-			// Ensure the vector is entirely along the floor plane.
-			playerToMouse.y = 0f;
-			
-			// Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
-			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-			
-			// Set the player's rotation to this new rotation.
-			playerRigidbody.MoveRotation(newRotation);
-		}
-
-	}
-	
 	void Facing()
 	{
 		
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	private float aux_angle = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	//	Application.LoadLevel ("RubenScene");
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		aux_angle += Time.deltaTime*90;
+		if (aux_angle > 360f) {
+			aux_angle -= 360f;
+		}
+
+		float aux_scale = 0.6f + 0.05f + Mathf.Sin (Mathf.Deg2Rad*aux_angle) * 0.05f;
+
+		this.gameObject.transform.FindChild("Logo1").transform.localScale = new Vector3(aux_scale, aux_scale, aux_scale);
+		this.gameObject.transform.FindChild("Logo2").transform.localScale = new Vector3(aux_scale, aux_scale, aux_scale);
+
+	}
+}
