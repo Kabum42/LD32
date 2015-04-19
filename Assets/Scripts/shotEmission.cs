9d3f5bf99ed252d077a3890ae125f3885f781a14
EtherealWhisper <yomowilce@gmail.com>
2015-04-20 00:41:53 +0200

    Blood script

--- Assets/Scripts/shotEmission.cs
+++ Assets/Scripts/shotEmission.cs
@@ -5,6 +5,7 @@ public class shotEmission : MonoBehaviour
 {
 	Transform myTransform;
 	GameObject myParticles;
+	GameObject myBlood;
 	// Use this for initialization
 	void Start ()
 	{
@@ -12,15 +13,20 @@ public class shotEmission : MonoBehaviour
 		
 		Vector3 direction = new Vector3 (1, 0, 0);
 		direction.Normalize ();
-		Vector3 position = new Vector3 (myTransform.position.x, myTransform.position.y + 1.89f, myTransform.position.z);
+		Vector3 position = new Vector3 (myTransform.position.x, myTransform.position.y + 2f, myTransform.position.z);
 		createEmission (direction, position);
+		createShot (direction, position);
 	}
 	
 	void createEmission (Vector3 direction, Vector3 position)
 	{
 		myParticles = (GameObject)(Instantiate (Resources.Load ("Blood"), new Vector3 (position.x, position.y, position.z), Quaternion.identity));
-
-		myParticles.transform.LookAt (myTransform.position + direction);
-
+		myParticles.transform.LookAt (myTransform.position + new Vector3 (0, 2, 0) + direction);
+	}
+	
+	void createShot (Vector3 direction, Vector3 position)
+	{
+		myBlood = (GameObject)(Instantiate (Resources.Load ("BloodSpatter"), new Vector3 (position.x, position.y, position.z), Quaternion.identity));
+		myBlood.transform.LookAt (myTransform.position + new Vector3 (0, 2, 0) + direction);
 	}
 }
